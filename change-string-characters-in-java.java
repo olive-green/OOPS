722ff97d521d35f93474de0369fc143f04e15453
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-08-23 01:35:10 +0530

    Add files via upload

---
+++ change-string-characters-in-java.java
@@ -0,0 +1,14 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+	    String s="abc";
+	    int i=0;
+	    s=s.substring(0,i)+'s'+s.substring(i+1);
+	    
+	    
+		System.out.println(s);
+	}
+}
