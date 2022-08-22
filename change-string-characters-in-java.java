54ccab28835c210e43e5fce3773dfbf5c1ccd006
Pankaj Rathore <65541151+olive-green@users.noreply.github.com>
2022-08-23 01:43:00 +0530

    Update change-string-characters-in-java.java

--- change-string-characters-in-java.java
+++ change-string-characters-in-java.java
@@ -7,6 +7,17 @@ class GFG {
 	    String s="abc";
 	    int i=0;
 	    s=s.substring(0,i)+'s'+s.substring(i+1);
+		
+	    //2nd method
+	    //convert string into char[]
+	    char[] temp=s.toCharArray();
+	    temp[i]='s';
+	    s=String.valueOf(temp);
+	    
+	    //using StringBuilder
+	    StringBuilder temp=new StringBuilder(s);
+	    temp.setCharAt(i,'s');
+	    s=temp.toString();
 	    
 	    
 		System.out.println(s);
